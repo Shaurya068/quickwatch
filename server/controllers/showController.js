26d6b024b352c7f140f03a8fd06724c67421c8c4
Shaurya068 <shauryadwivedi256@gmail.com>
2025-09-29 00:48:16 +0530

    Fix import case sensitivity for Movie model in showController.js

--- server/controllers/showController.js
+++ server/controllers/showController.js
@@ -1,5 +1,5 @@
 import axios from "axios"
-import Movie from "../models/movie.js";
+import Movie from "../models/Movie.js";
 import Show from "../models/Show.js";
 //Api to get movies from tmdb api
 const getNowPlayingMovies = async (req, res) => {
